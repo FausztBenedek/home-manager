f141e73a6609d406f7179df815f7f62a52caadb2
Benedek Fauszt <benedek.fauszt@kuehne-nagel.com>
2025-03-15 23:19:35 +0100

    Adjust default all options to be false

--- default-options.nix
+++ default-options.nix
@@ -5,8 +5,8 @@
   option.cli.cloud.aws.enable = false;
   option.cli.tools.safe-rm.enable = false;
   option.editor.jetbrains.enable = false;
-  option.mac.aerospace.enable = true;
-  option.mac.maccy.enable = true;
+  option.mac.aerospace.enable = false;
+  option.mac.maccy.enable = false;
   git = {
     name = "FausztBenedek";
     email = "fausztb@gmail.com";

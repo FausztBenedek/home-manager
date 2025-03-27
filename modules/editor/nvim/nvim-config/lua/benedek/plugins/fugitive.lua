35d9b21a0100f567f7b3700ab7d00d44d9ce6a9d
Benedek Fauszt <benedek.fauszt@kuehne-nagel.com>
2025-03-27 13:30:12 +0100

    Add shortcuts for fugitive

--- modules/editor/nvim/nvim-config/lua/benedek/plugins/fugitive.lua
+++ modules/editor/nvim/nvim-config/lua/benedek/plugins/fugitive.lua
@@ -1,3 +1,24 @@
 return {
   "tpope/vim-fugitive",
+  config = function()
+    vim.api.nvim_set_keymap(
+      "n",
+      "<leader>gb",
+      ":<C-U>Git blame<CR>",
+      { noremap = true, desc = "Open git blame", silent = true }
+    )
+
+    vim.keymap.set("n", "<leader>gh", function()
+      vim.cmd("Git log --decorate --oneline --graph %")
+    end, { noremap = true, desc = "Show commits that changed the file", silent = true })
+
+    vim.keymap.set("n", "<leader>gH", function()
+      vim.cmd("Git log --decorate --oneline --graph")
+    end, { noremap = true, desc = "Show commits", silent = true })
+
+    vim.keymap.set("n", "<leader>gd", function()
+      local commit_hash = vim.fn.getreg('"')
+      vim.cmd("Gvdiffsplit " .. commit_hash)
+    end, { noremap = true, desc = "Diff with commit hash in the \" register", silent = true })
+  end,
 }

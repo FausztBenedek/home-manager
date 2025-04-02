a137b1b6263185bce68213fb99c8e66f5fa7c160
Benedek Fauszt <benedek.fauszt@kuehne-nagel.com>
2025-04-02 10:57:53 +0200

    Copy the file's name in nvim

--- modules/editor/nvim/nvim-config/lua/benedek/core/keymaps.lua
+++ modules/editor/nvim/nvim-config/lua/benedek/core/keymaps.lua
@@ -13,16 +13,37 @@ vim.api.nvim_set_keymap("n", "<leader>sphu", ":<c-u>set spell spelllang=hu<CR>",
 vim.api.nvim_set_keymap("n", "<leader>spen", ":<c-u>set spell spelllang=en<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>spde", ":<c-u>set spell spelllang=de<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>spde", ":<c-u>set spell spelllang=de<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>aicg', ':lua require("benedek.own.grammar-check.grammar-check").show_info()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<leader>aicg', ':<C-U>lua require("benedek.own.grammar-check.grammar-check").show_info()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>aicg",
+  ':lua require("benedek.own.grammar-check.grammar-check").show_info()<CR>',
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "v",
+  "<leader>aicg",
+  ':<C-U>lua require("benedek.own.grammar-check.grammar-check").show_info()<CR>',
+  { noremap = true, silent = true }
+)
+vim.keymap.set("n", ",cc", function()
+  vim.fn.setreg("*", vim.fn.expand("%"))
+end, { noremap = true, silent = true, desc = "Copy current file's path relative to the project root" })
 
-vim.keymap.set('n', '<leader>vo', function()
-  local file_path = vim.fn.expand('%:p')
-  local line_number = vim.fn.line('.')
-  local command = string.format('/Applications/IntelliJ\\ IDEA.app/Contents/MacOS/idea --line %d %s', line_number, file_path)
+vim.keymap.set("n", ",cp", function()
+  vim.fn.setreg("*", vim.fn.expand("%:p"))
+end, { noremap = true, silent = true, desc = "Copy current file's full path" })
+
+vim.keymap.set("n", ",cn", function()
+  vim.fn.setreg("*", vim.fn.expand("%:t"))
+end, { noremap = true, silent = true, desc = "Copy current file's name" })
+
+vim.keymap.set("n", "<leader>vo", function()
+  local file_path = vim.fn.expand("%:p")
+  local line_number = vim.fn.line(".")
+  local command =
+    string.format("/Applications/IntelliJ\\ IDEA.app/Contents/MacOS/idea --line %d %s", line_number, file_path)
   print(command)
   vim.fn.system(command)
 end, { noremap = true, silent = true })
-
 
 vim.cmd("source $HM/vim/common-keymaps.vim")

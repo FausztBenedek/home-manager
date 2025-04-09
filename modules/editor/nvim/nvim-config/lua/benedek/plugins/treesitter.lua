efa932e4e3f0bc98173f7b776bcb92ca7b696b7b
Benedek Fauszt <benedek.fauszt@kuehne-nagel.com>
2025-04-09 16:07:15 +0200

    Remove unnecessary plugin "nvim-treesitter/nvim-treesitter-refactor"

--- modules/editor/nvim/nvim-config/lua/benedek/plugins/treesitter.lua
+++ modules/editor/nvim/nvim-config/lua/benedek/plugins/treesitter.lua
@@ -4,7 +4,6 @@ return {
   build = ":TSUpdate",
   dependencies = {
     "windwp/nvim-ts-autotag",
-    "nvim-treesitter/nvim-treesitter-refactor",
   },
   config = function()
     -- import nvim-treesitter plugin

2c0baaf6c61d2d9b176aaf9c61ffd9ae6a6b91fc
Benedek Fauszt <benedek.fauszt@kuehne-nagel.com>
2025-04-14 00:14:20 +0200

    Commit only zsh.nix, so that file history remains after mv

--- modules/cli/zsh.nix
+++ modules/cli/zsh.nix
@@ -1,19 +1,31 @@
-{ lib, pkgs, self, ... }:
+{ config, lib, pkgs, self, ... }:
 
 {
-  nix = {
-    package = pkgs.nix;
-    settings.experimental-features = [ "nix-command" "flakes" ];
+  options = {
+    option.starship.config-location = lib.mkOption {
+      type = lib.types.path;
+      description = ''
+        The location of the starship configuration. It defaults to copying the config to the store,
+        and symlinking there. If it is set, chainging the starship config has immediate effect.
+        If it is left default, home-manager switch is needed beforehand.
+      '';
+      default = ./starship.toml;
+    };
   };
-  home.stateVersion = "23.11";
 
-  home.sessionVariables = {
-    HM = lib.mkDefault "${self}";
-  };
+  config = {
+    nix = {
+      package = pkgs.nix;
+      settings.experimental-features = [ "nix-command" "flakes" ];
+    };
+    home.stateVersion = "23.11";
 
-  home.file = {
-    ".zshrc.d/vi-cursor-setter.zsh" = {
-      text = ''
+    home.sessionVariables = {
+      HM = lib.mkDefault "${self}";
+    };
+
+    home.file = {
+      ".zshrc.d/vi-cursor-setter.zsh".text = ''
         # Change cursor shape for different vi modes.
         function zle-keymap-select {
           if [[ ''${KEYMAP} == vicmd ]] ||
@@ -36,74 +48,76 @@
         echo -ne '\e[5 q' # Use beam shape cursor on startup.
         preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
       '';
+      ".config/starship.toml".source = config.lib.file.mkOutOfStoreSymlink config.option.starship.config-location;
     };
-  };
 
-  programs = {
-    home-manager.enable = true;
-    zsh = {
-      enable = true;
-      enableCompletion = true;
-      syntaxHighlighting.enable = true;
-      history = {
-        size = 10000;
-      };
-      plugins = [
-        {
-          name = "zsh-fzf-history-search"; # WORKAROUND in the zshrc for keybinds
-          src = pkgs.zsh-fzf-history-search;
-          file = "share/zsh-fzf-history-search/zsh-fzf-history-search.plugin.zsh";
-        }
-        {
-          name = "zsh-autosuggestions";
-          src = pkgs.zsh-autosuggestions;
-          file = "share/zsh-autosuggestions/zsh-autosuggestions.zsh";
-        }
-        # TODO https://github.com/softmoth/zsh-vim-mode
-      ];
-      oh-my-zsh = {
+    programs = {
+      home-manager.enable = true;
+      starship.enable = true;
+      zsh = {
         enable = true;
-        plugins = [ "git" ];
-        theme = "agnoster";
+        enableCompletion = true;
+        syntaxHighlighting.enable = true;
+        history = {
+          size = 10000;
+        };
+        plugins = [
+          {
+            name = "zsh-fzf-history-search"; # WORKAROUND in the zshrc for keybinds
+            src = pkgs.zsh-fzf-history-search;
+            file = "share/zsh-fzf-history-search/zsh-fzf-history-search.plugin.zsh";
+          }
+          {
+            name = "zsh-autosuggestions";
+            src = pkgs.zsh-autosuggestions;
+            file = "share/zsh-autosuggestions/zsh-autosuggestions.zsh";
+          }
+          # TODO https://github.com/softmoth/zsh-vim-mode
+        ];
+        oh-my-zsh = {
+          enable = true;
+          plugins = [ "git" ];
+          theme = "agnoster";
+        };
+        initExtra = ''
+          # Unbind control + right and left arrow coming from oh-my-zsh in $ZSH/lib/key-bindings.zsh
+          bindkey -r '^[[1;5C'
+          bindkey -r '^[[1;5D'
+
+          # Rebind keys correctly
+          bindkey -M emacs ';3C' forward-word
+          bindkey -M viins ';3C' forward-word
+          bindkey -M vicmd ';3C' forward-word
+          bindkey -M emacs ';3D' backward-word
+          bindkey -M viins ';3D' backward-word
+          bindkey -M vicmd ';3D' backward-word
+
+          # vi mode
+          bindkey -v
+          export KEYTIMEOUT=1
+          # Use vim keys in tab complete menu:
+          bindkey -M menuselect 'h' vi-backward-char
+          bindkey -M menuselect 'k' vi-up-line-or-history
+          bindkey -M menuselect '^p' vi-up-line-or-history
+          bindkey -M menuselect 'l' vi-forward-char
+          bindkey -M menuselect 'j' vi-down-line-or-history
+          bindkey -M menuselect '^n' vi-down-line-or-history
+
+          for config in ~/.zshrc.d/*.zsh; do
+            source $config
+          done
+
+          # Workaround for zsh-fzf-history-search plugin, because the default keymap was only available for emacs mode
+          bindkey -M emacs '^R' fzf_history_search
+          bindkey -M vicmd '^R' fzf_history_search
+          bindkey -M viins '^R' fzf_history_search
+
+          # Edit line in vim with ctrl-e:
+          bindkey -M emacs '^e' edit-command-line
+          bindkey -M vicmd '^e' edit-command-line
+          bindkey -M viins '^e' edit-command-line
+        '';
       };
-      initExtra = ''
-        # Unbind control + right and left arrow coming from oh-my-zsh in $ZSH/lib/key-bindings.zsh
-        bindkey -r '^[[1;5C'
-        bindkey -r '^[[1;5D'
-
-        # Rebind keys correctly
-        bindkey -M emacs ';3C' forward-word
-        bindkey -M viins ';3C' forward-word
-        bindkey -M vicmd ';3C' forward-word
-        bindkey -M emacs ';3D' backward-word
-        bindkey -M viins ';3D' backward-word
-        bindkey -M vicmd ';3D' backward-word
-
-        # vi mode
-        bindkey -v
-        export KEYTIMEOUT=1
-        # Use vim keys in tab complete menu:
-        bindkey -M menuselect 'h' vi-backward-char
-        bindkey -M menuselect 'k' vi-up-line-or-history
-        bindkey -M menuselect '^p' vi-up-line-or-history
-        bindkey -M menuselect 'l' vi-forward-char
-        bindkey -M menuselect 'j' vi-down-line-or-history
-        bindkey -M menuselect '^n' vi-down-line-or-history
-
-        for config in ~/.zshrc.d/*.zsh; do
-          source $config
-        done
-
-        # Workaround for zsh-fzf-history-search plugin, because the default keymap was only available for emacs mode
-        bindkey -M emacs '^R' fzf_history_search
-        bindkey -M vicmd '^R' fzf_history_search
-        bindkey -M viins '^R' fzf_history_search
-
-        # Edit line in vim with ctrl-e:
-        bindkey -M emacs '^e' edit-command-line
-        bindkey -M vicmd '^e' edit-command-line
-        bindkey -M viins '^e' edit-command-line
-      '';
     };
   };
 

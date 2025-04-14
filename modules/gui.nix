b1832d7690acdc0359ed61a4087067fdc885257c
Benedek Fauszt <benedek.fauszt@kuehne-nagel.com>
2025-04-14 12:17:34 +0200

    Commit only gui.nix, so that file history remains after mv

--- modules/gui.nix
+++ modules/gui.nix
@@ -7,6 +7,16 @@
       description = "Enable mac specific tools";
       default = false;
     };
+    option.alacritty.config-location = lib.mkOption {
+
+      type = lib.types.path;
+      description = ''
+        The location of the alacritty configuration. It defaults to copying the config to the store,
+        and symlinking there. If it is set, chainging the alacritty config has immediate effect.
+        If it is left default, home-manager switch is needed beforehand.
+      '';
+      default = ./alacritty/alacritty.toml;
+    };
 
   };
   config = lib.mkIf config.option.gui.enable {
@@ -22,76 +32,12 @@
       idea = "\"/Applications/IntelliJ IDEA.app/Contents/MacOS\"/idea";
       webstorm = "open -a WebStorm .";
     };
+    home.file = {
+      ".config/alacritty/alacritty.toml".source = config.lib.file.mkOutOfStoreSymlink config.option.nvim.config-location;
+    };
 
     programs = {
-      alacritty = {
-        # Alacritty only worked on mac so far, TODO Make it work on linux
-        enable = true;
-        #settings.font.normal.family = "ZedMono Nerd Font Mono"; # My previous fond
-        settings.font.normal.family = "JetBrainsMono Nerd Font";
-        settings.font.size = 14;
-        settings.window.decorations = "Buttonless";
-        settings.window.opacity = 0.85;
-        settings.window.blur = true;
-        settings.colors = {
-          primary =
-            {
-              background = "#282726";
-              foreground = "#FFFCF0";
-
-              dim_foreground = "#FFFCF0";
-              bright_foreground = "#FFFCF0";
-              # dim_background = "#1C1B1A"; Reported as unused key
-              # bright_background = "#1C1B1A"; Reported as unused key
-            };
-
-          # Cursor colors
-          cursor =
-            {
-              text = "#FFFCF0";
-              cursor = "#FFFCF0";
-            };
-
-          # Normal colors
-          normal =
-            {
-              black = "#100F0F";
-              red = "#AF3029";
-              green = "#66800B";
-              yellow = "#AD8301";
-              blue = "#205EA6";
-              magenta = "#A02F6F";
-              cyan = "#24837B";
-              white = "#FFFCF0";
-            };
-
-          # Bright colors
-          bright =
-            {
-              black = "#100F0F";
-              red = "#D14D41";
-              green = "#879A39";
-              yellow = "#D0A215";
-              blue = "#4385BE";
-              magenta = "#CE5D97";
-              cyan = "#3AA99F";
-              white = "#FFFCF0";
-            };
-
-          # Dim colors
-          dim =
-            {
-              black = "#100F0F";
-              red = "#AF3029";
-              green = "#66800B";
-              yellow = "#AD8301";
-              blue = "#205EA6";
-              magenta = "#A02F6F";
-              cyan = "#24837B";
-              white = "#FFFCF0";
-            };
-        };
-      };
+      alacritty.enable = true;
     };
   };
 }

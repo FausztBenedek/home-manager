f6a2dd93b57025040438539df8242357aae9de08
FausztBenedek <fausztb@gmail.com>
2025-04-20 01:56:12 +0200

    Change cursor to Numix-Cursor

--- modules/linux/compositor/hyprland/hyprland-module.nix
+++ modules/linux/compositor/hyprland/hyprland-module.nix
@@ -1,4 +1,7 @@
 { config, pkgs, lib, ... }:
+let
+  cursorSize = 40;
+in
 {
   options = {
     option.linux.compositor.hyprland = lib.mkEnableOption "Use hyprland as compositor";
@@ -24,27 +27,19 @@
       iconTheme.name = "Numix-Circle";
       # iconTheme.package = pkgs.numix-icon-theme-circle;
     };
-    home.pointerCursor =
-      let
-        getFrom = url: hash: name: {
-          gtk.enable = true;
-          x11.enable = true;
-          name = name;
-          size = 48;
-          package =
-            pkgs.runCommand "moveUp" { } ''
-              mkdir -p $out/share/icons
-              ln -s ${pkgs.fetchzip {
-                url = url;
-                hash = hash;
-              }} $out/share/icons/${name}
-            '';
-        };
-      in
-      getFrom
-        "https://github.com/ful1e5/fuchsia-cursor/releases/download/v2.0.0/Fuchsia-Pop.tar.gz"
-        "sha256-BvVE9qupMjw7JRqFUj1J0a4ys6kc9fOLBPx2bGaapTk="
-        "Fuchsia-Pop";
+    home.sessionVariables = {
+      XCURSOR_SIZE = cursorSize;
+    };
+    home.pointerCursor = {
+      enable = true;
+      name = "Numix-Cursor";
+      gtk.enable = true;
+      hyprcursor = { enable = true; size = cursorSize; };
+      x11.enable = true;
+      dotIcons.enable = true;
+      size = cursorSize;
+      package = pkgs.numix-cursor-theme;
+    };
 
   };
 }

75cdbeffc8048883f0e5fc194d719410e0e2fdd4
Benedek Fauszt <benedek.fauszt@kuehne-nagel.com>
2025-05-02 09:53:56 +0200

    Start command in insert mode

--- modules/cli/shell/zsh-module.nix
+++ modules/cli/shell/zsh-module.nix
@@ -26,27 +26,8 @@
 
     home.file = {
       ".zshrc.d/vi-cursor-setter.zsh".text = ''
-        # Change cursor shape for different vi modes.
-        function zle-keymap-select {
-          if [[ ''${KEYMAP} == vicmd ]] ||
-             [[ $1 = 'block' ]]; then
-            echo -ne '\e[1 q'
-          elif [[ ''${KEYMAP} == main ]] ||
-               [[ ''${KEYMAP} == viins ]] ||
-               [[ ''${KEYMAP} = \'\' ]] ||
-               [[ $1 = 'beam' ]];
-          then
-            echo -ne '\e[5 q'
-          fi
-        }
-        zle -N zle-keymap-select
-        zle-line-init() {
-          #zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
-          echo -ne "\e[5 q"
-        }
-        zle -N zle-line-init
-        echo -ne '\e[5 q' # Use beam shape cursor on startup.
-        preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
+        # Always starting with insert mode for each command line
+        ZVM_LINE_INIT_MODE=$ZVM_MODE_INSERT
       '';
       ".config/starship.toml".source = config.lib.file.mkOutOfStoreSymlink config.option.starship.config-location;
     };
